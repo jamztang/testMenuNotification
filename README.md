cd54f4bec369fa0b25859c7ff5b6e3f054cda998
James Tang <james@goodnotesapp.com>
2019-10-16 14:19:44 +0800

    Add UIMenuController.willHideMenuNotification and other notifications

---
+++ README.md
@@ -0,0 +1,4 @@
+testMenuNotification
+---
+
+A sample project to verify `UIMenuController.didHideMenuNotification` and `UIMenuController.willHideMenuNotification` never gets called in Catalyst app
--- testMenuNotification/ViewController.swift
+++ testMenuNotification/ViewController.swift
@@ -8,11 +8,70 @@
 
 import UIKit
 
+class MenuController: UIMenuController {
+    override func hideMenu() {
+        super.hideMenu()
+    }
+
+    override func hideMenu(from targetView: UIView) {
+        super.hideMenu(from: targetView)
+    }
+}
+
 class ViewController: UIViewController {
+
+    let longPressGesture = UILongPressGestureRecognizer()
+    let menuController = MenuController()
+    var willShowMenuNotificationObserver: Any!
+    var willHideMenuNotificationObserver: Any!
+    var menuFrameDidChangeNotificationObserver: Any!
+    var didHideMenuNotificationObserver: Any!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+        longPressGesture.addTarget(self, action: #selector(handleLongPressGesture(_:)))
+        view.addGestureRecognizer(longPressGesture)
+
+        willShowMenuNotificationObserver = NotificationCenter.default.addObserver(forName: UIMenuController.willShowMenuNotification, object: nil, queue: OperationQueue.main) { (notification) in
+            Swift.print("LLL willShowMenuNotification")
+        }
+
+        willHideMenuNotificationObserver = NotificationCenter.default.addObserver(forName: UIMenuController.willHideMenuNotification, object: nil, queue: OperationQueue.main) { (notification) in
+            Swift.print("LLL willHideMenuNotification")
+        }
+
+        didHideMenuNotificationObserver = NotificationCenter.default.addObserver(forName: UIMenuController.didHideMenuNotification, object: nil, queue: OperationQueue.main) { (notification) in
+            Swift.print("LLL didHideMenuNotification")
+        }
+
+        menuFrameDidChangeNotificationObserver = NotificationCenter.default.addObserver(forName: UIMenuController.menuFrameDidChangeNotification, object: nil, queue: OperationQueue.main) { (notification) in
+            Swift.print("LLL menuFrameDidChangeNotification")
+        }
+    }
+
+    @objc func handleLongPressGesture(_ longPressGesture: UILongPressGestureRecognizer) {
+
+        guard longPressGesture.state == .began else {
+            return
+        }
+
+        let point = longPressGesture.location(in: self.view)
+        let rect = CGRect(x: point.x, y: point.y, width: 10, height: 10)
+
+        Swift.print("LLL longPress \(point)")
+
+        menuController.showMenu(from: self.view, rect: rect)
+
+    }
+
+    override var canBecomeFirstResponder: Bool {
+        return true
+    }
+
+    override func copy(_ sender: Any?) {
+        Swift.print("copy")
     }
 
 
