00b4be99e1d0a79ba4eadaa51051a6d19f7886be
James Tang <james@goodnotesapp.com>
2019-10-16 13:44:28 +0800

    Initial Commit

---
+++ testMenuNotification/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testMenuNotification
+//
+//  Created by James Tang on 16/10/2019.
+//  Copyright © 2019 James Tang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
